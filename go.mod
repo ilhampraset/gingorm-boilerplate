738892cda6c2051c9df20298852ca3dd63d7c5fa
ilhampraset <ilhampraset30@gmail.com>
2020-08-22 23:19:48 +0700

    global config

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.12
 
 require (
 	github.com/gin-gonic/gin v1.6.3
+	github.com/ilyakaznacheev/cleanenv v1.2.5
 	github.com/jinzhu/gorm v1.9.16
+	github.com/joho/godotenv v1.3.0
 )
--- main.go
+++ main.go
@@ -1,23 +1,64 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/ilhampraset/gingorm-boilerplate/config"
 	"github.com/ilhampraset/gingorm-boilerplate/controllers"
-	"github.com/ilhampraset/gingorm-boilerplate/models"
+	"github.com/ilhampraset/gingorm-boilerplate/providers"
+	"github.com/ilyakaznacheev/cleanenv"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+
+	"os"
 )
 
+type Args struct {
+	ConfigPath string
+}
+
+var cfg config.Config
+
 func init() {
-	models.InitDb()
+
+	args := ProcessArgs(&cfg)
+
+	if err := cleanenv.ReadConfig(args.ConfigPath, &cfg); err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+	providers.InitDb(&cfg)
+
 }
 
 func main() {
 
 	app := gin.Default()
+
 	app.GET("/people", controllers.GetPeople)
 	app.GET("/people/:id", controllers.GetPerson)
 	app.POST("/people", controllers.CreatePerson)
 	app.PUT("/people/:id", controllers.UpdatePerson)
 	app.DELETE("/people/:id", controllers.DeletePerson)
 	app.Run()
+}
+
+// ProcessArgs processes and handles CLI arguments
+func ProcessArgs(cfg *config.Config) Args {
+	var a Args
+
+	f := flag.NewFlagSet("Example server", 1)
+	f.StringVar(&a.ConfigPath, "c", ".env", "Path to configuration file")
+
+	fu := f.Usage
+	f.Usage = func() {
+		fu()
+		envHelp, _ := cleanenv.GetDescription(cfg, nil)
+
+		fmt.Fprintln(f.Output())
+		fmt.Fprintln(f.Output(), envHelp)
+	}
+
+	f.Parse(os.Args[1:])
+	return a
 }
